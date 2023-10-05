68caa71f60d4c8dbb5d84be4f68546480ee52432
Sien Khumaen Damarwendha <143293717+Damarwendha@users.noreply.github.com>
2023-10-05 08:57:25 +0700

    Added toFixed

--- #4 Tip Calculator/src/components/output/Output.jsx
+++ #4 Tip Calculator/src/components/output/Output.jsx
@@ -2,12 +2,11 @@ export default function Output({ states }) {
   function percentage(firstTip, secondTip, bill) {
     return bill * ((firstTip + secondTip) / 2 / 100);
   }
-  const totalTip = percentage(states.firstTip, states.secondTip, states.bill);
+  const totalTip = percentage(states.firstTip, states.secondTip, states.bill).toFixed(2);
   return (
     <p>
       <strong>
-        You pay ${Number(states.bill) + totalTip} (${states.bill} + ${totalTip}{" "}
-        tip)
+        You pay ${Number(states.bill) + totalTip} (${states.bill} + ${totalTip} tip)
       </strong>
     </p>
   );
