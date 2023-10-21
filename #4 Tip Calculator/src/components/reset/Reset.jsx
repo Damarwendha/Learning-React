778e73ca5728ef689227d7e67d0b94b202963c64
Sien Khumaen Damarwendha <143293717+Damarwendha@users.noreply.github.com>
2023-10-21 19:01:28 +0700

    Delete #4 Tip Calculator directory

--- #4 Tip Calculator/src/components/reset/Reset.jsx
+++
@@ -1,9 +0,0 @@
-export default function Reset({ states }) {
-  const resetHandler = () => {
-    states.setBill("");
-    states.setFirstTip(0);
-    states.setSecondTip(0);
-  };
-
-  return <button onClick={resetHandler}>Reset</button>;
-}
